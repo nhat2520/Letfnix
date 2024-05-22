152a331e810d8b5b28228c09098c667ca409d7e5
chirikatori <thanhluffy5D@gmail.com>
2024-05-22 19:01:09 +0700

    Add HomePage

--- server/app/urls.py
+++ server/app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('helloworld', views.helloworld, name="helloworld"),
     path('login/', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
-    path('register', views.register_view, name='register')
+    path('register', views.register_view, name='register'),
+    path('movie/<str:pk>', views.movie, name="movie"),
+    path('search', views.search_by_name, name="search")
 ]
