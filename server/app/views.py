be64ae955a69270be3e8dd92ea93b196401f9930
chirikatori <thanhluffy5D@gmail.com>
2024-05-22 12:57:32 +0700

    Update requirements.txt

--- server/app/views.py
+++ server/app/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 # Create your views here.
 def helloworld(request):
+    return render(request, 'helloworld.html', {})
+
+
+def home(request):
     return render(request, 'home.html', {})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request=request, user=user)
+            messages.success(request, "You have been logged in...")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error, ...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('home')
