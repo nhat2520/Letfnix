29153a7f0d5caa76a7aa165f08b49e7d409b6c00
chirikatori <thanhluffy5D@gmail.com>
2024-05-22 12:58:49 +0700

    Add auth path using django.contrib.auth

--- server/app/urls.py
+++ server/app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('helloworld', views.helloworld, name="helloworld"),
-    path('login', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('register', views.register_view, name='register')
 ]
--- server/app/views.py
+++ server/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
+from .forms import RegisterForm
+
 
 # Create your views here.
 def helloworld(request):
@@ -32,3 +34,24 @@ def logout_view(request):
     logout(request)
     messages.success(request, "You have been logged out...")
     return redirect('home')
+
+
+def register_view(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+
+            user = authenticate(username=username,
+                                password=password)
+            login(request, user)
+            messages.success(request, "You have registered successfully")
+            return redirect('home')
+        else:
+            messages.success(request, "There have been a problem")
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {"form": form})
