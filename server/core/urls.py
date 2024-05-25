27dab8ad6053081c6a275e7326ff0cb588d19432
chirikatori <thanhluffy5D@gmail.com>
2024-05-25 17:19:01 +0700

    Fix "url" payment

--- server/core/urls.py
+++ server/core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("app.urls")),
     path('cart/', include("cart.urls")),
-    path('payments/ ', include("payments.urls")),
+    path('payment/ ', include("payment.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
