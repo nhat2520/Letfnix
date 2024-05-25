9158da41ddec48ed7828f65bcc621cbee10f9045
CoderHung <22022662@vnu.edu.vn>
2024-05-25 15:02:34 +0700

    updated recommend code

--- recommend/recommend.py
+++ recommend/recommend.py
@@ -2,6 +2,7 @@
 
 import pickle
 import warnings
+import os
 
 import numpy as np
 import pandas as pd
@@ -24,17 +25,19 @@ def create_and_save_tfidf_matrix_v1():
         tfidf_v1.pkl: Pickle file containing the fitted TfidfVectorizer model.
         tfidf_matrix_v1.npz: NPZ file containing the TF-IDF matrix.
     """
-    data = pd.read_csv('data.csv')
+    directory = "server/repo"  
+    os.makedirs(directory, exist_ok=True)
+    data = pd.read_csv('recommend/data.csv')
     data['tagline'] = data['tagline'].fillna('')
     data['description'] = data['overview'] + data['tagline']
     data['description'] = data['description'].fillna('')
     tf = TfidfVectorizer(
         analyzer="word", ngram_range=(1, 2), min_df=0.01, stop_words="english"
     )
-    tfidf_matrix = tf.fit_transform(documents['description'])
-    with open("tfidf_v1.pkl", "wb") as f:
+    tfidf_matrix = tf.fit_transform(data['description'])
+    with open("server/repo/tfidf_v1.pkl", "wb") as f:
         pickle.dump(tf, f)
-    save_npz("tfidf_matrix_v1.npz", tfidf_matrix)
+    save_npz("server/repo/tfidf_matrix_v1.npz", tfidf_matrix)
 
 
 # Function to create a new user profile and save it
@@ -50,9 +53,9 @@ def create_and_save_user_profile(user_id, tfidf_matrix_filename):
         user_profile_{user_id}.pkl: Pickle file containing the user profile vector.
     """
 
-    tfidf_matrix = load_npz(f"tfidf_matrix_{tfidf_matrix_filename}.npz")
+    tfidf_matrix = load_npz(f"server/repo/tfidf_matrix_{tfidf_matrix_filename}.npz")
     user_profile = np.mean(tfidf_matrix.toarray(), axis=0)
-    with open(f"user_profile_{user_id}.pkl", "wb") as f:
+    with open(f"server/repo/user_profile_{user_id}.pkl", "wb") as f:
         pickle.dump(user_profile, f)
 
 
@@ -69,12 +72,12 @@ def calculate_and_save_similarity_matrix(user_id, tfidf_matrix_filename):
         sim_matrix_{user_id}.npz: NPZ file containing the cosine similarity matrix.
     """
 
-    with open(f"user_profile_{user_id}.pkl", "rb") as f:
+    with open(f"server/repo/user_profile_{user_id}.pkl", "rb") as f:
         user_profile = pickle.load(f)
-    tfidf_matrix = load_npz(f"tfidf_matrix_{tfidf_matrix_filename}.npz")
+    tfidf_matrix = load_npz(f"server/repo/tfidf_matrix_{tfidf_matrix_filename}.npz")
     user_profile = user_profile.reshape(1, -1)
     cosine_sim = linear_kernel(user_profile, tfidf_matrix)
-    save_npz(f"sim_matrix_{user_id}.npz", csr_matrix(cosine_sim))
+    save_npz(f"server/repo/sim_matrix_{user_id}.npz", csr_matrix(cosine_sim))
 
 
 # Function to get recommendations based on a user_id
@@ -89,21 +92,21 @@ def get_recommendations(user_id):
         list of int: The indices of the top 5 recommended items.
     """
 
-    cosine_sim = load_npz(f"sim_matrix_{user_id}.npz").toarray()
+    cosine_sim = load_npz(f"server/repo/sim_matrix_{user_id}.npz").toarray()
     top_n_indices = cosine_sim[0].argsort()[-5:][::-1]  # Top 5 recommendations
     return top_n_indices
 
 
 # Function to update user profile and cosine similarity matrix
 def update_user_and_similarity_matrix(
-    user_id, movie_idx, action, tfidf_matrix_filename
+    user_id, movie_name, action, tfidf_matrix_filename
 ):
     """
     Update the user profile and cosine similarity matrix based on user actions.
 
     Args:
         user_id (int): The user ID for which to update the profile and similarity matrix.
-        movie_idx (int): The index of the movie in the TF-IDF matrix.
+        movie_name (str): The name of the movie.
         action (str): The action performed by the user ('view' or 'buy').
         tfidf_matrix_filename (str): The filename of the saved TF-IDF matrix.
     """
@@ -117,19 +120,22 @@ def update_user_and_similarity_matrix(
         user_profile = user_profile * (1 - weight) + tfidf_vector * weight
         return user_profile / np.linalg.norm(user_profile)
 
-    with open(f"user_profile_{user_id}.pkl", "rb") as f:
+    with open(f"server/repo/user_profile_{user_id}.pkl", "rb") as f:
         user_profile = pickle.load(f)
-        
-    tfidf_matrix = load_npz(f"tfidf_matrix_{tfidf_matrix_filename}.npz")
+    
+    data = pd.read_csv('recommend/data.csv')
+    movie_idx =  data.index[data['original_title'] == movie_name].tolist()
+
+    tfidf_matrix = load_npz(f"server/repo/tfidf_matrix_{tfidf_matrix_filename}.npz")
     movie_tfidf_vector = tfidf_matrix[movie_idx].toarray().flatten()
     user_profile = update_user_profile(user_profile, movie_tfidf_vector, action)
 
-    with open(f"user_profile_{user_id}.pkl", "wb") as f:
+    with open(f"server/repo/user_profile_{user_id}.pkl", "wb") as f:
         pickle.dump(user_profile, f)
 
     user_profile = user_profile.reshape(1, -1)
     updated_cosine_sim = linear_kernel(user_profile, tfidf_matrix)
-    save_npz(f"sim_matrix_{user_id}.npz", csr_matrix(updated_cosine_sim))
+    save_npz(f"server/repo/sim_matrix_{user_id}.npz", csr_matrix(updated_cosine_sim))
 
 
 
